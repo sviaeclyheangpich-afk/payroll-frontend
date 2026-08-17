439043b9368ce3ec983bb56de72e75358ad067aa
LyheangDev <lyheangdev@gmail.com>
2026-08-17 13:19:00 +0700

    feat: imoplment alert and modal

---
+++ app/components/base/Input.vue
@@ -0,0 +1,17 @@
+<template>
+    <input :disabled="disabled" autoComplete="additional-name" :aria-label="ariaLabel"
+        class="w-full focus:border-green-500 focus:outline-0 focus:ring-0 border text-sm text-gray-200 border-gray-50/10 rounded-xs bg-white/2 py-1.5 pl-10 pr-2.5 placeholder:text-[13px] placeholder:text-gray-300"
+        :type="type" :placeholder="placeholder" :maxLength="maxLength" :required="required" />
+</template>
+
+<script setup lang="ts">
+interface InputProps {
+    type?: 'text' | 'email' | 'password';
+    placeholder?: string;
+    required?: boolean;
+    maxLength?: number;
+    ariaLabel?: string;
+    disabled?: boolean;
+}
+defineProps<InputProps>();
+</script>
